f5a0b077f1d131711cd02dd8b5e12e4e8f6d8846
Dmitrii642 <dmit.iakovlev@gmail.com>
2024-02-07 22:36:43 +0300

    Add history of operations by TableView

--- TinkoffCalculator/CalculationListViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CalculationListViewController.swift
-//  TinkoffCalculator
-//
-//  Created by Дмитрий Яковлев on 07.02.2024.
-//
-
-import Foundation
---
+++ TinkoffCalculator/History/HistoryTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  HistoryTableViewCell.swift
+//  TinkoffCalculator
+//
+//  Created by Dmitrii Iakovlev on 07.02.2024.
+//
+
+import Foundation
